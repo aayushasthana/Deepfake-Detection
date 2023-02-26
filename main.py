d835d27c0466fecf6c45d52e86ce82171b5eb35a
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2023-02-26 01:32:17 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,144 @@
-# This is a sample Python script.
-
-from signature_utils import *
-from video_utils import *
-from encrypt_utils import *
-import argparse
 import time
-import copy
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import rsa
+
+# Globals
+TILE_SIZE = 48
+
+def read_video(video_name):
+    frames = []
+    cap = cv2.VideoCapture(video_name)
+    if not cap.isOpened():
+        print("Error opening video file {}".format(video_name))
+    count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    try:
+        for i in range(0, count):
+            ret, f = cap.read()
+            frames.append(f)
+    except Exception as e:
+        print(str(e))
+
+    cap.release()
+    return count, width, height, fps, frames
 
 
-TILE_SIZE = 8
-
-def cli_options():
-    # construct the argument parse and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-v", "--video", required=True,
-                    help="path of the video name")
-    ap.add_argument("-n","--number", required=True,
-                    help="first n frames ")
-
-    args = vars(ap.parse_args())
-
-    return args
+def display_tile(frames, frame_number, row, col, title):
+    plt.imshow(frames[frame_number][row:row + TILE_SIZE, col:col + TILE_SIZE])
+    plt.title(title + "-f#{} r#{} c#{}".format(frame_number, row, col))
+    plt.show()
 
 
-# Press the green button in the gutter to run the script.
+def dump_tile(frames, frame_number, row, col, title):
+    print(title + "-f#{} r#{} c#{}".format(frame_number, row, col))
+    print(frames[frame_number][row:row + TILE_SIZE, col:col + TILE_SIZE])
+
+
+def Avg_tile(frames, frame_number, row, col):
+    tile = np.array(frames[frame_number][row:row + TILE_SIZE, col:col + TILE_SIZE])
+    R = tile[:, :, 0]
+    R46 = R[1:-1, 1:-1]
+
+    G = tile[:, :, 1]
+    G46 = G[1:-1, 1:-1]
+
+    B = tile[:, :, 2]
+    B46 = B[1:-1, 1:-1]
+
+    return int(np.average(R46)), int(np.average(G46)), int(np.average(B46))
+
+
+def display_frame(frames, frame_number, title):
+    plt.figure()
+    plt.imshow(cv2.cvtColor(frames[frame_number], cv2.COLOR_BGR2RGB))
+    plt.title(title + "-f#{}".format(frame_number))
+    plt.show()
+
+
+def create_signature(avgR, avgG, avgB, frame_number, row, col):
+    data_string = str(avgR) + str(avgG) + str(avgB) + str(frame_number) + str(row * col)
+    return data_string
+
+
+def asymmetric_encrypt(data, publicKey):
+    encrypt_data = rsa.encrypt(data.encode(), publicKey)
+    return encrypt_data
+
+
+def asymmetric_decrypt(encrypted_data, privateKey):
+    decrypted_data = rsa.decrypt(encrypted_data, privateKey).decode()
+    return decrypted_data
+
+
+def get_keys():
+    with open(
+            r"publicKey.pem", "rb"
+    ) as key:
+        publicKey = rsa.PublicKey.load_pkcs1(key.read())
+    with open(
+            r"privateKey.pem", "rb"
+    ) as key:
+        privateKey = rsa.PrivateKey.load_pkcs1(key.read())
+    return publicKey, privateKey
+
+#Embedding
+def embedSignatureInTile(tile, signature):
+    mask1 = [224, 28, 3]
+    mask2 = [31, 227, 252]
+    # mask = [b'11100000', b'00011100', b'00000011']
+    # mask2 = [b'00011111', b'11100011', b'11111100']
+
+    for color in range(0, 3):
+        transfer = np.bitwise_and(signature, mask1[color])
+        transfer2 = np.bitwise_and(tile[0, 8:40, color], mask2[color])
+        tile[0, 8:40, color] = np.bitwise_or(transfer2, transfer)
+
+    return tile
+
+
+def extractSignature(tile):
+    extracted_signature = np.array(bytearray(32))
+    mask = [224, 28, 3]
+
+    for color in range(0, 3):
+        transfer = np.bitwise_and(tile[0, 8:40, color], mask[color])
+        extracted_signature = np.bitwise_or(extracted_signature, transfer)
+
+    return extracted_signature
+
+
 if __name__ == '__main__':
+    videos = ["man_face.mp4", "woman_face.mp4"]
+    frame_count, w, h, fps, frames = read_video(videos[0])
+    print("Framecount: {}, Width: {}, Height: {}, FPS: {}\n".format(frame_count, w, h, fps))
 
-    args = cli_options()
+    startTime = time.time()
+    for frame_number in range(0, len(frames)):
+        prev = time.time()
+        for row in range(0, h, TILE_SIZE):
+            for col in range(0, w, TILE_SIZE):
+                # Signature creation
+                AvgR, AvgG, AvgB = Avg_tile(frames, frame_number, row, col)
+                signature = create_signature(AvgR, AvgG, AvgB, frame_number, row, col)
 
-    video_name = args["video"]
-    first_n_frames = int(args["number"])
+                # Encrypting the signature
+                publicKey, privateKey = get_keys()
+                encryptedSignature = asymmetric_encrypt(signature, publicKey)
 
-    frame_count, w, h, fps, frames = read_video(video_name)
-    print("Video:{} Frame_count:{} Width:{} Height:{} FPS:{}".format(video_name, frame_count, w, h, fps))
-    title = video_name + "pre-processing"
+                # Embedding the signature
+                currTile = np.array(frames[frame_number][row:row + TILE_SIZE, col:col + TILE_SIZE])
+                encryptedSignature = np.array(list(encryptedSignature), dtype=int).reshape(32)
+                currTile = np.array(embedSignatureInTile(currTile, encryptedSignature))
 
+                # Extracting the signature
+                extractedSignature = extractSignature(currTile)
+                decryptedSignature = asymmetric_decrypt(extractedSignature, privateKey)
 
+        print(f"Frame Number: {frame_number+1}, Total Time: {time.time()-startTime}, Split: {time.time()-prev}")
 
-    show_videos_from_frames(frames, title)
-
-    original_frame_0 = copy.copy(frames[0])
-
-    named_tuple = time.localtime()  # get struct_time
-    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
-    print("Starting processing ...{} ".format(time_string))
-    row_count = int(w/TILE_SIZE)
-    col_count = int(h/TILE_SIZE)
-    for f in range(0,first_n_frames):
-        start_time = time.time()
-        for row in range(0,row_count):
-            for col in range(0,col_count):
-                AssignSignature_perTile(frames,f,row,col,f, row,col,)
-        end_time = time.time()
-        print("{}-{} seconds".format(f, end_time-start_time))
-
-    title = video_name + "post-processing"
-    #show_videos_from_frames(frames,title)
-
-    named_tuple = time.localtime()  # get struct_time
-    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
-    print("Ending processing ...{} ".format(time_string))
-
-    show_frames_side_by_side(0,original_frame_0,"original",frames[0],"embedded signature")
-
-
-
-
-
-
+    endTime = time.time()
+    print(endTime - startTime)
