dc4af8a0a4f337a0a72327b1721500b1835136bf
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2023-03-10 09:53:25 -0800

    latest with face dectectio failure

--- face_detect.py
+++ face_detect.py
@@ -22,27 +22,36 @@ def detectFaceHaarCascade(frames, frame_count):
     return faces
 
 def detectFaceMTCNN(frames, frame_count):
+    tqdm_value = frame_count
+    progress_bar = tqdm(desc="Detecting Face", total=tqdm_value)
+
     mtcnn = MTCNN(margin=0,thresholds=[0.85, 0.95, 0.95])
     face_tiles = []
     row = -1
     col = -1
-    for  frame in range(0, frame_count):
+    failed_detection_count = 0
+    for  i in range(0, frame_count):
 
         # Detect faces
         try:
+            f = cv2.cvtColor(frames[i], cv2.COLOR_BGR2RGB)
 
-            boxes, _ = mtcnn.detect(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            print(boxes)
+            boxes, _ = mtcnn.detect(f)
             topX = boxes[0][0]
             topY = boxes[0][1]
             bottomX = boxes[0][2]
             bottomY = boxes[0][3]
-            row = topX / TILE_SIZE
-            col = topY / TILE_SIZE
+            row = int(topY) + TILE_SIZE - int(topY) % TILE_SIZE
+            col = int(topX) + TILE_SIZE - int(topX) % TILE_SIZE
+            #print (f"X:{topX} C:{col} Y:{topY} R:{row}")
         except:
-            print("failed to detect")
-        face_tiles.append((row,col))
+            print("*")
+            failed_detection_count += 1
+        progress_bar.update(1)
 
-    return face_tiles
+
+        face_tiles.append((i,row,col))
+
+    return face_tiles, failed_detection_count
 
 
